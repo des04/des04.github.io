b65eff31b401638b80dba13f7fa0a11a4f2d1b2a
Desmond <43046919+des04@users.noreply.github.com>
2020-01-30 23:57:13 -0600

    Add files via upload

--- TDBB/js.js
+++ TDBB/js.js
@@ -1,1 +1,27 @@
-﻿
+﻿function showEnglish()
+{
+    var x = document.getElementById("english");
+    if (x.style.display != "block") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function showMath()
+{
+    var x = document.getElementById("math");
+    if (x.style.display != "block") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function showScience()
+{
+    var x = document.getElementById("science");
+    if (x.style.display != "block") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
